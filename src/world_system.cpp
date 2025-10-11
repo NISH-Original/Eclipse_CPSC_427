85dd054f9ac9893ac53f64325a5d8aeb4600f368
Ina <nakyounginalee@gmail.com>
2025-10-10 22:37:22 -0700

    Added hardcoded slime spawns

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -291,13 +291,14 @@ void WorldSystem::restart_game() {
 
 	// TODO: remove hardcoded enemy creates
 	glm::vec2 player_init_position = { window_width_px/2, window_height_px - 200 };
-	createEnemy(renderer, { player_init_position.x + 300, player_init_position.y + 300 });
-	createEnemy(renderer, { player_init_position.x - 300, player_init_position.y + 300 });
-	createEnemy(renderer, { player_init_position.x + 300, player_init_position.y - 300 });
-	createEnemy(renderer, { player_init_position.x - 300, player_init_position.y - 300 });
-	createEnemy(renderer, { player_init_position.x + 300, player_init_position.y });
-	createEnemy(renderer, { player_init_position.x - 300, player_init_position.y });
-	createEnemy(renderer, { player_init_position.x, player_init_position.y + 300 });
+	createSlime(renderer, { player_init_position.x + 250, player_init_position.y + 250 });
+	createSlime(renderer, { player_init_position.x - 250, player_init_position.y + 250 });
+	createEnemy(renderer, { player_init_position.x + 250, player_init_position.y - 250 });
+	createEnemy(renderer, { player_init_position.x - 250, player_init_position.y - 250 });
+	createEnemy(renderer, { player_init_position.x + 250, player_init_position.y });
+	createEnemy(renderer, { player_init_position.x - 250, player_init_position.y });
+	createSlime(renderer, { player_init_position.x, player_init_position.y + 250 });
+	createEnemy(renderer, { player_init_position.x, player_init_position.y - 250 });
 }
 
 // Compute collisions between entities
