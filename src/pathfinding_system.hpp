cba8937646744285c9cba24e0d50725739fdb6c6
yzheng50 <yzheng50@students.cs.ubc.ca>
2025-10-31 01:14:24 -0700

    Redesign the pathfinding code

---
+++ src/pathfinding_system.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "tiny_ecs_registry.hpp"
+#include "common.hpp"
+
+using PathVector = glm::ivec2;
+
+constexpr int FIELD_RADIUS = 16;
+constexpr int FIELD_SIZE = FIELD_RADIUS * 2;
+constexpr PathVector DIRECTIONS[] = {
+	{ 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
+	{ 1, 1 }, { 1, -1 }, { -1, 1 }, { -1,-1 }
+};
+constexpr int CARDINAL_COST = 10;
+constexpr int DIAGONAL_COST = 14;
+
+struct PathNode {
+	int cost = std::numeric_limits<int>::infinity();
+	PathVector dir{ 0, 0 };
+	bool walkable = true;
+};
+
+class PathfindingSystem {
+public:
+	PathfindingSystem() : flow_field(FIELD_SIZE, std::vector<PathNode>(FIELD_SIZE)) {}
+
+	void step(float elapsed_ms);
+
+private:
+	std::vector<std::vector<PathNode>> flow_field;
+
+	void build_flow_field();
+};
