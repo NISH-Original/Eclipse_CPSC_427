bd56f6bbd1269b0ac4546a05009e873f2172e89e
yzheng50 <yzheng50@students.cs.ubc.ca>
2025-10-31 10:31:46 -0700

    Partially implemented steering system

--- src/components.hpp
+++ src/components.hpp
@@ -99,6 +99,15 @@ struct AccumulatedForce {
 	glm::vec2 v{ 0, 0 };
 };
 
+struct Steering {
+	float target_angle;
+	float rad_ms = 0.005;
+};
+
+struct AccumulatedForce {
+	glm::vec2 v{ 0, 0 };
+};
+
 // Cooldown timer for taking damage (prevents continuous damage)
 struct DamageCooldown {
 	float cooldown_ms = 0.f;
